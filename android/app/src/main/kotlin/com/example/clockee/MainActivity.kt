606156682c44905c7be1d0d01020641af3577822
Shashi2632 <120296568+Shashi2632@users.noreply.github.com>
2024-07-04 14:07:38 +0530

    deleted

--- android/app/src/main/kotlin/com/example/clockee/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clockee
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/global.dart
+++
@@ -1,3 +0,0 @@
-// Supported audio player file formats
-const livestreamFormats = ["m3u8", "mpd"];
-const audioFormats = ["mp3", "wav", "m4a", "aac"];
